20882bc03e177acfe9b1c5e5d1191daa9cc9f30f
200web <200webstudio@gmail.com>
2024-10-15 10:00:01 +0200

    reviews

--- src/sections/Reviews.jsx
+++ src/sections/Reviews.jsx
@@ -1,15 +1,9 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import appStyle from "../scss/app.module.scss";
 import like from "../assets/img/like.webp";
 
 const Reviews = ({ selectedLocale }) => {
-  const [currentIndex, setCurrentIndex] = React.useState(0);
-  const [isAnimating, setIsAnimating] = React.useState(false);
-  const [isPaused, setIsPaused] = React.useState(false);
-  const firstRowRef = useRef(null);
-  const secondRowRef = useRef(null);
-
   const [reviewsData, setReviewsData] = useState([
     {
       name: "Дмитрий",
@@ -29,11 +23,11 @@ const Reviews = ({ selectedLocale }) => {
     {
       name: "Игорь",
       position: "Интернет-магазин одежды",
-      description: "Ребята из Mercury Arts помогли нам настроить конверсионные кампании в Instagram и Facebook. Продажи выросли, планируем расширять ассортимент товарок на рекламу. Отличная работа!",
+      description: "Ребята из Mercury Arts помогли нам настроить конверсионные кампании в Instagram и Facebook. Продажи выросли, планируем расширять ассортимент товаров на рекламу. Отличная работа!",
     },
   ]);
 
-  const [reviewsTitle, setReviewsTitle] = useState(''); // Добавляем состояние для заголовка
+  const [reviewsTitle, setReviewsTitle] = useState(''); // Заголовок отзывов
 
   const fetchReviewsData = async () => {
     try {
@@ -49,7 +43,7 @@ const Reviews = ({ selectedLocale }) => {
       const fetchedData = result.data;
 
       // Устанавливаем заголовок отзывов
-      setReviewsTitle(fetchedData.Reviews_title || 'Наши отзывы'); // Используем условие для установки заголовка
+      setReviewsTitle(fetchedData.Reviews_title || 'Наши отзывы');
 
       // Обновляем данные отзывов с учетом данных из API
       const updatedReviewsData = [
@@ -78,7 +72,6 @@ const Reviews = ({ selectedLocale }) => {
       setReviewsData(updatedReviewsData);
     } catch (error) {
       console.error("Ошибка запроса данных:", error);
-      // В случае ошибки оставляем русские варианты
       setReviewsData((prevData) => prevData);
     }
   };
@@ -87,54 +80,20 @@ const Reviews = ({ selectedLocale }) => {
     fetchReviewsData();
   }, [selectedLocale]);
 
-  // Дублируем отзывы для бесконечного прокрута
-  const duplicatedReviewsData = [...reviewsData, ...reviewsData];
-
-  useEffect(() => {
-    let interval;
-
-    if (!isPaused) {
-      interval = setInterval(() => {
-        setIsAnimating(true);
-        setCurrentIndex((prevIndex) => {
-          const nextIndex = (prevIndex + 1) % duplicatedReviewsData.length;
-          setTimeout(() => {
-            setIsAnimating(false);
-          }, 3);
-          return nextIndex;
-        });
-      }, 3); // Установите желаемый интервал между сменой отзывов
-    }
-
-    return () => clearInterval(interval);
-  }, [isPaused, duplicatedReviewsData.length]);
-
-  const handleMouseEnter = () => {
-    setIsPaused(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsPaused(false);
-  };
-
-  const currentReview = duplicatedReviewsData[currentIndex];
+  // Разбиваем отзывы на два ряда
+  const firstRowReviews = reviewsData.slice(0, 2);
+  const secondRowReviews = reviewsData.slice(2, 4);
 
   return (
-    <div className={appStyle.rewievsBlock}>
+    <div className={appStyle.reviewsBlock}>
       <div className={appStyle.Row}>
         <label>{reviewsTitle || 'Наши отзывы'}</label>
       </div>
 
-      
-      <div className={appStyle.rewievsCarousel}>
-        <div className={appStyle.firstVertRow} ref={firstRowRef}>
-          {duplicatedReviewsData.map((review, index) => (
-            <div
-              key={index}
-              className={appStyle.card}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
+      <div className={appStyle.reviewsStatic}>
+        <div className={appStyle.firstRow}>
+          {firstRowReviews.map((review, index) => (
+            <div key={index} className={appStyle.card}>
               <div className={appStyle.reviewCardHeader}>
                 <div className={appStyle.like}>
                   <img draggable="false" src={like} alt="like" />
@@ -150,14 +109,9 @@ const Reviews = ({ selectedLocale }) => {
             </div>
           ))}
         </div>
-        <div className={appStyle.secondVertRow} ref={secondRowRef}>
-          {duplicatedReviewsData.map((review, index) => (
-            <div
-              key={index}
-              className={appStyle.card}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
+        <div className={appStyle.secondRow}>
+          {secondRowReviews.map((review, index) => (
+            <div key={index} className={appStyle.card}>
               <div className={appStyle.reviewCardHeader}>
                 <div className={appStyle.like}>
                   <img draggable="false" src={like} alt="like" />
